dcb20b6a2fbb3df33903559369269450c8e9d942
pchandt <pcthakuri26@gmail.com>
2022-07-21 15:06:55 -0700

    Line-at-a-time vs single string.

--- src/main/java/com/tlglearning/wordcount/Main.java
+++ src/main/java/com/tlglearning/wordcount/Main.java
@@ -1,28 +1,31 @@
 package com.tlglearning.wordcount;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Main {
 
   private static final String TEST_FILE_NAME = "hound-of-the-baskervilles.txt";
 
-  public static void main(String[] args) throws URISyntaxException, IOException {
-    URI uri = Main.class
-        .getClassLoader()
-        .getResource(TEST_FILE_NAME)
-        .toURI();
-    Path path = Paths.get(uri);
-    String text = Files.readString(path);
-    WordCounter counter= new WordCounter(text);
+  public static void main(String[] args) throws IOException {
+    try(
+        InputStream input = Main.class.getClassLoader().getResourceAsStream(TEST_FILE_NAME);
+        Reader reader = new InputStreamReader(input);
+        BufferedReader buffer =  new BufferedReader(reader)
+    ) {
+      WordCounter counter = new WordCounter();
+      String line;
+      while ((line = buffer.readLine()) != null) {
+        // TODO Pass line to a method of WordCounter.
+        counter.add(line);
+      }
+      // TODO Do something with the WordCounter.
+      System.out.println(counter);
+    }
 
-    // TODO do something with counter.
-    System.out.println(counter);
   }
 
 }
