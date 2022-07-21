c4b08a4add17ca81ea35b2dd95fee5231ea4c2d2
pchandt <pcthakuri26@gmail.com>
2022-07-21 15:30:52 -0700

    Line-at-a-time vs single string.

--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -5,27 +5,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public final class WordCounter {
+public class WordCounter {
 
-  private final Map<String, Integer> counts;
+  private final Map<String, Integer> counts = new HashMap<>();
 
-  public WordCounter(String text) {
-    String[] words = splitWords(text);
-    counts = Collections.unmodifiableMap(countWords(words));
-  }
+  private int totalWords;
 
-  public Set<String> words(){
+
+  public Set<String> words() {
     return counts.keySet();
-
   }
 
-  public int getCount(String word){
+  public int get(String word) {
     return counts.getOrDefault(word, 0);
   }
 
   public Map<String, Integer> getCounts() {
-    return counts;
+    return Collections.unmodifiableMap(counts);
   }
+
+  public void add(String text) {
+    String trimmedLine = text.trim();
+    if (!trimmedLine.isEmpty()) {
+      String[] words = splitWords(trimmedLine);
+      countWords(words);
+    }
+  }
+
+  public int size(){
+    return counts.size();
+  }
+   public int total(){
+    return totalWords;
+   }
 
   @Override
   public String toString() {
@@ -38,22 +50,14 @@ public final class WordCounter {
         .split("[\\W_]+");
   }
 
-  Map<String, Integer> countWords(String[] words) {
-    Map<String, Integer> counts = new HashMap<>();
+  void countWords(String[] words) {
     for (String word : words) {
       // DONE Check if word is already present as a key in counts;
       //  if it's not present, add it to counts with a value of 1;
       //  otherwise, get the current value, add 1 to it, and update the map with the new value.
-
-      if (!counts.containsKey(word)) {
-        counts.put(word, 1);
-      } else {
-        int previousCount = counts.get(word);
-        counts.put(word, previousCount + 1);
-      }
-
+      counts.put(word, get(word) + 1);
+      totalWords++;
     }
-    return counts;
   }
 
 
