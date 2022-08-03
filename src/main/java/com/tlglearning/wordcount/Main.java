fe02193b36fbff6f0851012ae4726ba561852f3a
pchandt <pcthakuri26@gmail.com>
2022-08-03 15:27:44 -0700

    Added javadoc (in progress).

--- src/main/java/com/tlglearning/wordcount/Main.java
+++ src/main/java/com/tlglearning/wordcount/Main.java
@@ -6,13 +6,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Comparator;
-import java.util.Map;
 import java.util.Map.Entry;
 
-public class Main {
+/**
+ * Demonstrates the practice of word counting, used (for example) in authorship studies.
+ * This demonstration uses (int eh included methods) implementations of the {@link java.util.Map},
+ * {@link java.util.Map.Entry}, and {@link java.util.stream.Stream} interfaces.
+ */
+
+public final class Main {
 
   private static final String TEST_FILE_NAME = "hound-of-the-baskervilles.txt";
 
+private Main(){
+
+}
+  /**
+   * Creates an instance of {@link WordCounter}, using it to process the contents of "The Hound of
+   * the Baskervilles", by Arthur Conan Doyle. After processing, the word counts (excluding short
+   * words) are listed in descending order.
+   * <p>This method assumes that a text file name {@code hound-of-the-baskervilles.txt} is located
+   * ont he classpath; otherwise (or if that file cannot be read for some reason), an instance of
+   * {@link IOException} is thrown.</p>
+   *
+   * @param args Command-line arguments (currently ignored)
+   * @throws IOException Thrown if input file cannot be located or read.
+   */
   public static void main(String[] args) throws IOException {
     try (
         InputStream input = Main.class.getClassLoader().getResourceAsStream(TEST_FILE_NAME);
@@ -29,11 +48,10 @@ public class Main {
           .getCounts()
           .entrySet()
           .stream()
-          .sorted(Comparator.comparing(Entry<String, Integer>:: getValue).reversed())
+          .sorted(Comparator.comparing(Entry<String, Integer>::getValue).reversed())
           .limit(10)
           .forEach((entry) -> System.out.println(entry));
 //                .forEach(System.out::println);
-
 
     }
 
